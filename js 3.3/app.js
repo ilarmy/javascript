09de30a043c9dac342c1e89f570d211f8ae41155
ilarmy <151742698+ilarmy@users.noreply.github.com>
2024-01-11 19:52:10 +0400

    Add files via upload

---
+++ js 3.3/app.js
@@ -0,0 +1,9 @@
+let gender = prompt('Enter the gender');
+let user= prompt("Enter your name")
+if (gender === "female") {
+  alert( 'Your gender the female Ilahe');
+} else if (gender === "male") {
+  alert( 'Your gender the male Ilahe');
+} else {
+  alert( 'Invalid gender' );
+}
