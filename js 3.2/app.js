ee26649203ef5628a6dfd89810ac5d2157f9ba9f
ilarmy <151742698+ilarmy@users.noreply.github.com>
2024-01-11 18:32:03 +0400

    Add files via upload

---
+++ js 3.2/app.js
@@ -0,0 +1,9 @@
+let year_month = prompt('Enter the month number');
+
+if (year_month == 1) {
+  alert( 'Bu ay yanvar ayidir' );
+} else if (year_month ==12) {
+  alert( 'Bu ay dekabr ayidir' );
+} else {
+  alert( 'Ilin diger ayidir' );
+}
