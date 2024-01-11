a8036c0af7969c5c292b2a67e4011777f341fd17
ilarmy <151742698+ilarmy@users.noreply.github.com>
2024-01-11 17:11:50 +0400

    Add files via upload

---
+++ js 3.1/all.js
@@ -0,0 +1,9 @@
+let week = prompt('Enter the week');
+
+if (week == 1) {
+  alert( 'Bu gun bazar ertesidir' );
+} else if (week ==6) {
+  alert( 'Bu gun senbe gunudur' );
+} else {
+  alert( 'Hefte icindeyik' );
+}
