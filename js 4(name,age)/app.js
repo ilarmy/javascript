303451850c15dfd21fe06d30549f58fcb168f45c
ilarmy <151742698+ilarmy@users.noreply.github.com>
2024-02-08 16:14:40 +0400

    Add files via upload

---
+++ js 4(name,age)/app.js
@@ -0,0 +1,8 @@
+let name = prompt("Enter the name");
+let gender = prompt("Enter the gender");
+let age = +prompt("Enter the age")
+if (gender==="woman"&& (age>18)){
+    alert("Salam xanim" )
+}else{
+    alert("Icaze yoxdur")
+}
