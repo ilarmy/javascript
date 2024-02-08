9e5ce1bfc29b20d6394f4688016054c6d93a1736
ilarmy <151742698+ilarmy@users.noreply.github.com>
2024-02-08 13:05:00 +0400

    Add files via upload

---
+++ js 1 (if)/app.js
@@ -0,0 +1,18 @@
+let hefteninGunu= +prompt("Enter the day");
+if (hefteninGunu === 1){
+    alert("Bu gun bazar ertesidir ");
+}else if (hefteninGunu===2 ){
+    alert("Bu gun cersenbe axsamidir")
+}else if(hefteninGunu===3){
+    alert("Bu gun cersenbedir")
+}else if(hefteninGunu===4){
+    alert("Bu gun cume axsamidir")
+}else if(hefteninGunu===5){
+    alert("Bu gun cumedir")
+}else if(hefteninGunu===6){
+    alert("Bu gun senbedir")
+}else if (hefteninGunu===7){
+    alert("Bu gun bazar gunudur")
+}else{
+    alert("Bele bir hefte gunu yoxdur")
+}
