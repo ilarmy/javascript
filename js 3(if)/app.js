d523897001367ecbb47f968126467f9160407115
ilarmy <151742698+ilarmy@users.noreply.github.com>
2024-02-08 15:21:38 +0400

    Add files via upload

---
+++ js 3(if)/app.js
@@ -0,0 +1,8 @@
+let gender= prompt("Enter the gender");
+if (gender==="women"){
+    alert("Gender qadindir");
+}else if(gender==="man"){
+    alert("Gender kisidir");
+}else{
+    alert("Bele bir gender yoxdur");
+}
